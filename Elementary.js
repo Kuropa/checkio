3ef5480470f3ab0b772ef282da4ef742f2a46bcb
Svetlana Vargo <drumminde@MacBook-Pro-Svetlana.local>
2020-03-11 17:24:02 +0300

    Fizz Buzz

--- Elementary.js
+++ Elementary.js
@@ -1,16 +1,39 @@
+//Say Hi
 // В этой миссии вы должны написать функцию, которая представит человека по переданным параметрам.
 // Входные данные: Два аргумента строка(str) и положительное число(int)
 const sayHi = (str, int) => {
     return 'Hi. My name is ' + str + ' and I\'m ' + int + ' years old';
 }
-console.log(sayHi("Sveta", 26));
 
+//Index Power
 //Дан массив с положительными числами и число N. Вы должны найти N-ую степень элемента в массиве с индексом N.
 const indexPower = (arr, int) => {
     for (let i = 0; i < arr.length; i++) {
         if (i === int) {
-            return arr[i] * i;
+            return Math.pow(arr[i], int);
+        }
+        if (arr.length - 1 < int) {
+            return -1;
         }
     }
 };
-console.log(indexPower([1, 2, 3, 4], 2));
+
+//Fizz Buzz
+// Вы должны написать функцию, которая принимает положительное целое число и возвращает:
+// "Fizz Buzz", если число делится на 3 и 5;
+// "Fizz", если число делится на 3;
+// "Buzz", если число делится на 5;
+// Число, как строку для остальных случаев.
+// Входные данные: Число, как целочисленное (int).
+const fizzBuzz = (int) => {
+    if (int % 15 === 0) {
+        return 'Fizz Buzz';
+    }
+    if (int % 3 === 0) {
+        return 'Fizz';
+    }
+    if (int % 5 === 0){
+        return 'Buzz';
+    }
+    return String(int);
+};
