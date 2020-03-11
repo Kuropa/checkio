8b35b60d657fc7c5f3fe8b8ce95f539a3eb940da
Svetlana Vargo <drumminde@MacBook-Pro-Svetlana.local>
2020-03-11 17:05:29 +0300

    Index Power

--- Elementary.js
+++ Elementary.js
@@ -4,3 +4,13 @@ const sayHi = (str, int) => {
     return 'Hi. My name is ' + str + ' and I\'m ' + int + ' years old';
 }
 console.log(sayHi("Sveta", 26));
+
+//Дан массив с положительными числами и число N. Вы должны найти N-ую степень элемента в массиве с индексом N.
+const indexPower = (arr, int) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (i === int) {
+            return arr[i] * i;
+        }
+    }
+};
+console.log(indexPower([1, 2, 3, 4], 2));
