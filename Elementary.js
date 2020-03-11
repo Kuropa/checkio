979692ea8d2f5d152cfaf4cce3af646a7199161e
Svetlana Vargo <drumminde@MacBook-Pro-Svetlana.local>
2020-03-11 16:37:59 +0300

    Say Hi

--- Elementary.js
+++ Elementary.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+// В этой миссии вы должны написать функцию, которая представит человека по переданным параметрам.
+// Входные данные: Два аргумента строка(str) и положительное число(int)
+const sayHi = (str, int) => {
+    return 'Hi. My name is ' + str + ' and I\'m ' + int + ' years old';
+}
+console.log(sayHi("Sveta", 26));
